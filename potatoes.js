268eb00c659457b630e5630581f1cde3f6667a72
Franco Ortega <francopdx@gmail.com>
2022-11-08 22:28:02 -0800

    create allThePotatoes with incorrect second test

--- potatoes.js
+++ potatoes.js
@@ -6,3 +6,8 @@ const firstAnswer = allThePotatoes(1, 2, 3);
 
 if (firstAnswer === 6) console.log('First answer, CORRECT');
 else console.log('First answer, WRONG:', firstAnswer);
+
+const secondAnswer = allThePotatoes(4, 5, 6, 7);
+
+if (secondAnswer === 6) console.log('First answer, CORRECT');
+else console.log('First answer, WRONG:', secondAnswer);
