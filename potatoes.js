61b5fe066399bc31e11ee8fcc41b8a345d4b35af
Franco Ortega <francopdx@gmail.com>
2023-11-01 15:58:57 -0700

    create multiplyPotatoes

--- potatoes.js
+++ potatoes.js
@@ -6,6 +6,17 @@ const sumAllPotatoes = (nums) => {
   });
 };
 
+const multiplyPotatoes = (factors) => {
+  return factors.reduce((a, c) => {
+    if (typeof c === 'string') {
+      console.log('Error: not a number');
+      return;
+    }
+
+    return a * c;
+  });
+};
+
 /////////////////////
 // function to check the answer
 
@@ -21,3 +32,6 @@ console.log('testing...1, 2, 3...');
 
 checkAnswer(sumAllPotatoes, 6, [1, 2, 3]);
 checkAnswer(sumAllPotatoes, '1 2 3', ['1', '2', '3']);
+checkAnswer(multiplyPotatoes, 16, [2, 8]);
+checkAnswer(multiplyPotatoes, 24, [2, 3, 4]);
+checkAnswer(multiplyPotatoes, '24', ['2', '3', '4']);
