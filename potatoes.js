8cbde7a28ae2dc7b96ea8d920f5621e496be5138
Franco Ortega <francopdx@gmail.com>
2023-11-01 15:47:10 -0700

    remove old unused code

--- potatoes.js
+++ potatoes.js
@@ -20,32 +20,3 @@ const checkAnswer = (fn, answer, ...nums) => {
 console.log('testing...1, 2, 3...');
 
 checkAnswer(sumAllPotatoes, 6, 1, 2, 3);
-
-// const firstAnswer = allThePotatoes(1, 2, 3);
-// const firstExpected = 6;
-
-// if (firstAnswer === 6) console.log('First answer, CORRECT');
-// else console.log('First answer, WRONG:', firstAnswer);
-
-// const secondAnswer = allThePotatoes(4, 5, 6, 7);
-// const secondExpected = 22;
-
-// if (secondAnswer === 22) console.log('Second answer, CORRECT');
-// else console.log('Second answer, WRONG:', secondAnswer);
-
-// const countAllThePotatoes = (...potatoes) => {
-//   const count = potatoes.length;
-//   const total = allThePotatoes(...potatoes);
-
-//   return { count, total };
-// };
-
-// const potatoCountAndTotal = countAllThePotatoes(1, 2, 3);
-// console.log(potatoCountAndTotal);
-
-// const potatoWords = allThePotatoes(
-//   'french fries',
-//   'hash browns',
-//   'mashed potatoes'
-// );
-// console.log(potatoWords);
