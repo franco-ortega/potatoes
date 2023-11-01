704760e095356edd14a7ecae96011c3f7b897ddb
Franco Ortega <francopdx@gmail.com>
2023-11-01 15:42:10 -0700

    fix fn paramater used in function and comment out previous code

--- potatoes.js
+++ potatoes.js
@@ -10,38 +10,40 @@ const sumAllPotatoes = (...args) => {
 // function to check the answer
 
 const checkAnswer = (fn, answer, ...nums) => {
-  const fnAnswer = fun(nums);
+  const fnAnswer = fn(nums);
 
   if (fnAnswer === answer) console.log('Correct!');
   else console.log('Incorrect :(');
 };
 /////////////////////
 
-const firstAnswer = allThePotatoes(1, 2, 3);
-const firstExpected = 6;
+console.log('testing...1, 2, 3...');
 
-if (firstAnswer === 6) console.log('First answer, CORRECT');
-else console.log('First answer, WRONG:', firstAnswer);
+// const firstAnswer = allThePotatoes(1, 2, 3);
+// const firstExpected = 6;
 
-const secondAnswer = allThePotatoes(4, 5, 6, 7);
-const secondExpected = 22;
+// if (firstAnswer === 6) console.log('First answer, CORRECT');
+// else console.log('First answer, WRONG:', firstAnswer);
 
-if (secondAnswer === 22) console.log('Second answer, CORRECT');
-else console.log('Second answer, WRONG:', secondAnswer);
+// const secondAnswer = allThePotatoes(4, 5, 6, 7);
+// const secondExpected = 22;
 
-const countAllThePotatoes = (...potatoes) => {
-  const count = potatoes.length;
-  const total = allThePotatoes(...potatoes);
+// if (secondAnswer === 22) console.log('Second answer, CORRECT');
+// else console.log('Second answer, WRONG:', secondAnswer);
 
-  return { count, total };
-};
+// const countAllThePotatoes = (...potatoes) => {
+//   const count = potatoes.length;
+//   const total = allThePotatoes(...potatoes);
 
-const potatoCountAndTotal = countAllThePotatoes(1, 2, 3);
-console.log(potatoCountAndTotal);
+//   return { count, total };
+// };
 
-const potatoWords = allThePotatoes(
-  'french fries',
-  'hash browns',
-  'mashed potatoes'
-);
-console.log(potatoWords);
+// const potatoCountAndTotal = countAllThePotatoes(1, 2, 3);
+// console.log(potatoCountAndTotal);
+
+// const potatoWords = allThePotatoes(
+//   'french fries',
+//   'hash browns',
+//   'mashed potatoes'
+// );
+// console.log(potatoWords);
